b2c00eee1e328a91e474d5c88eeaaad90d8e37a1
v-irvan.halim@btpn.com <s9IhZBEb>
2018-04-24 15:09:34 +0700

    fix script

--- ws.groovy
+++ ws.groovy
@@ -1,6 +1,8 @@
 node{
+    def root = tool name: 'Go1.9.2', type: 'go'
 ws("${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_ID}/"){
-    withEnv(["GOPATH=${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_ID}"]){
+    withEnv(["GOROOT=${root}", "GOPATH=${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_ID}","PATH+GO=${root}/bin"]){
+        env.PATH="${GOPATH}/bin:$PATH"
         stage('Checkout') {
             checkout scm
         }
